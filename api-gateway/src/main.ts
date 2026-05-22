607c71f2c0738ded2334889f376531b793af66b0
ace-brown <kazem_abdi@yahoo.com>
2026-05-22 19:01:20 +0200

    Fix dist fodler

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
